c8d2031e16f44fb819a6705aa89f514346da7776
Marfullsen <Marfullsen@gmail.com>
2020-12-13 19:42:05 -0300

    Agrega y muestra notas

--- cli.py
+++ cli.py
@@ -7,8 +7,37 @@ from utils import clrscr
 # Constantes
 T_ARGS= len(argv) # Total de argumentos.
 ARGS = argv # Argumentos ingresados.
+DEFAULT_SECTION = 'TODO' # Sección por defecto.
 
-debug = True
+v_args = ['', 'add']
+
+
+debug = False
+libreta = {
+            DEFAULT_SECTION : 
+            {
+                1 : 
+                {
+                    'text' : 'Hello world!'
+                }
+            },
+            'Pruebas de la semana' :
+            {
+                1 : 
+                {
+                    'text' : 'lenguaje'
+                }
+            },
+            'Salidas de hoy' :
+            {
+                1 : {
+                    'text' : 'Ir al Kayak'
+                },
+                2 : {
+                    'text' : 'Ir a la playa'
+                }
+            },
+        }
 
 def betainfo(msg):
     if debug:
@@ -21,10 +50,43 @@ def test():
     for i, argumento in enumerate(argumentos):
         print(f'4rgum3nt0 {i}:', argumento)
 
-def menu():
-    print("Willkommen мой друг")
-    input("Type something: ")
+def show(seccionEspecifica=''):
+    clrscr()
+    for seccion in libreta:
+        betainfo(seccion)
+        print(seccion)
+        for nota in libreta[seccion]:
+            print(nota, libreta[seccion][nota]['text'])
+        print()
 
+
+def add(msj='', seccion=DEFAULT_SECTION):
+
+    T_NOTAS = len(libreta[seccion])
+    ID_NOTA_NUEVA = T_NOTAS + 1
+
+    libreta[seccion][ID_NOTA_NUEVA] = dict()
+    libreta[seccion][ID_NOTA_NUEVA]['text'] = msj
+
+    print('Nota agregada.')
+
+def menu():
+    print(
+    "Willkommen мой друг",
+    "Opciones disponibles:",
+    "1) MostraR LiBreTa",
+    "2) Añadir NoTa.",
+    "0) Salir",
+    sep='\n')
+
+    opc = input("Opcion: ")
+    if opc == '0':
+        exit()
+    elif opc == '1':
+        show()
+    elif opc == '2':
+        add(input('...: '))
+    menu()
 
 def main():
     # Comandos reconocidos / valid arguments.
@@ -32,16 +94,18 @@ def main():
     if T_ARGS == 1:
         betainfo('one argument')
         print("Libreta")
+        show()
     elif len(argv) > 1:
         betainfo('more than one argument')
         if argv[1] == 'menu':
             betainfo('recognized argument')
             if argv[1] in v_args:
-                v_args[1]
+                v_args[1]()
         else:
             print("Unrecognized yet:", argv[1])
     
 
 if __name__ == "__main__":    
+    if '-v' in argv: debug=True
     main()
     #test()
